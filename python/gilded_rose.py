bc50d60ced87ada291bfd00e8b00a7ff2887717f
Rebecca Jesson <rebecca.jesson@softwire.com>
2025-11-10 14:18:56 +0000

    Refactor
    Undo incorrect test fix

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,33 +7,32 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if "aged brie" not in item.name.lower() and "backstage passes" not in item.name.lower():
-                if item.quality > 0:
-                    if "sulfuras" not in item.name.lower():
-                        item.quality = item.quality - 1
+            delta_quality = -1
+            if "sulfuras" in item.name.lower():
+                continue
+            elif "aged brie" in item.name.lower():
+                delta_quality = 1
+            elif "backstage passes" in item.name.lower():
+                delta_quality = 1
+                if item.sell_in < 11:
+                    delta_quality += 1
+                if item.sell_in < 6:
+                    delta_quality += 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if "backstage passes" in item.name.lower():
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if "sulfuras" not in item.name.lower():
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if "aged brie" not in item.name.lower():
-                    if "backstage passes" not in item.name.lower():
-                        if item.quality > 0:
-                            if "sulfuras" not in item.name.lower():
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                if item.sell_in < 1:
+                    delta_quality -= 1
+
+            if "conjured" in item.name.lower():
+                delta_quality *= 2
+
+            item.quality += delta_quality
+
+            if item.quality > 50:
+                item.quality = 50
+            elif item.quality < 0:
+                item.quality = 0
+
+            item.sell_in -= 1
 
 
 class Item:
